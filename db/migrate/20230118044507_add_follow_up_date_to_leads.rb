214e7d7aade2ddad024099c555fb19a76cacb1fe
fernando zuniga <carlosfernando.zh@gmail.com>
2023-01-18 22:29:53 -0600

    migration add follow up date and time, added  fields to form

--- db/migrate/20230118044507_add_follow_up_date_to_leads.rb
+++ db/migrate/20230118044507_add_follow_up_date_to_leads.rb
@@ -1,5 +1,6 @@
 class AddFollowUpDateToLeads < ActiveRecord::Migration[7.0]
   def change
-    add_column :leads, :follow_up_date, :datetime
+    add_column :leads, :follow_up_date, :date
+    add_column :leads, :follow_up_time, :time
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_044507) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "comments"
-    t.datetime "follow_up_date"
+    t.date "follow_up_date"
+    t.time "follow_up_time"
     t.index ["customer_id"], name: "index_leads_on_customer_id"
     t.index ["user_id"], name: "index_leads_on_user_id"
   end
