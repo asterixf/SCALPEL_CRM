6b9a7a1f905fcde7d5b4df203904f581256e4830
fernando zuniga <carlosfernando.zh@gmail.com>
2023-02-01 23:00:41 -0600

    configured stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,11 @@
 // Entry point for the build script in your package.json
+
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+window.Stimulus = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+Stimulus.load(definitionsFromContext(context))
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
